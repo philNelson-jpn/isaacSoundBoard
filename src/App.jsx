5a90f2a8842840b1bf64d5383f05d0b3ad1b33fb
Phil Nelson <nelsonpa@miamioh.edu>
2023-12-17 17:47:26 +0900

    update picture divs to interactive buttons add landing hero image

--- src/App.jsx
+++ src/App.jsx
@@ -9,78 +9,90 @@ function App() {
 			source: '/images/BeltSwing.jpeg',
 			alt: 'isaac swinging a belt',
 			audioFile: '/sounds/broughtToYouBy.mp3',
+			caption: 'ME',
 		},
 		{
 			id: crypto.randomUUID(),
 			source: '/images/broughtToYouBy.jpeg',
 			alt: 'startled isaac with mustache and HIM hat',
 			audioFile: '/sounds/notRecommended.mp3',
+			caption: 'AAAAAGGHH',
 		},
 		{
 			id: crypto.randomUUID(),
 			source: '/images/chamboFremb.jpeg',
 			alt: 'half naked viking isaac',
 			audioFile: '/sounds/noLongerAtTheBar.mp3',
+			caption: 'At The Bar',
 		},
 		{
 			id: crypto.randomUUID(),
 			source: '/images/christmasisaac.jpeg',
 			alt: 'isaac with santa hat and dog',
 			audioFile: '/sounds/OuchAudio.mp4',
+			caption: 'OW',
 		},
 		{
 			id: crypto.randomUUID(),
 			source: '/images/devilsTesticle.jpeg',
 			alt: 'beared isaac peering into your soul',
 			audioFile: '/sounds/diviningRod.mp3',
+			caption: 'Divining Rod',
 		},
 		{
 			id: crypto.randomUUID(),
 			source: '/images/haveYouSeenMyBicycle.jpeg',
 			alt: 'isaac on a buffalo',
 			audioFile: '/sounds/GlassPitAudio.mp4',
+			caption: 'Glass Pit',
 		},
 		{
 			id: crypto.randomUUID(),
 			source: '/images/noLongerAtTheBar.jpeg',
 			alt: 'neckless flannel shirt isaac',
 			audioFile: '/sounds/jingleMyBells.mp3',
+			caption: 'Jingle Bells',
 		},
 		{
 			id: crypto.randomUUID(),
 			source: '/images/notRecommended.jpeg',
 			alt: 'ddr screaming isaac',
 			audioFile: '/sounds/devilsTesticle.mp3',
+			caption: "Devil's Testicle",
 		},
 		{
 			id: crypto.randomUUID(),
 			source: '/images/theManWithManyHats.jpeg',
 			alt: 'isaac with many hats',
 			audioFile: '/sounds/chamboFrembNew.mp3',
+			caption: 'Chambo Fremb',
 		},
 		{
 			id: crypto.randomUUID(),
 			source: '/images/teefs.jpeg',
 			alt: 'toothless isaac',
 			audioFile: '/sounds/thisAsshole.mp3',
+			caption: 'This Asshole',
 		},
 		{
 			id: crypto.randomUUID(),
 			source: '/images/IsaacBirdman.png',
-			alt: 'toothless isaac',
+			alt: 'Isaac as a bird',
 			audioFile: '/sounds/BirdmanAudio.mp4',
+			caption: 'Screeeeech',
 		},
 		{
 			id: crypto.randomUUID(),
 			source: '/images/IsaacCondom.png',
-			alt: 'toothless isaac',
+			alt: 'condom balloon',
 			audioFile: '/sounds/CondomAudio.mp4',
+			caption: 'Expands',
 		},
 	]
 
 	const [selectedAudio, setSelectedAudio] = React.useState(null)
 	const [selectedImage, setSelectedImage] = React.useState(
-		'/images/haveYouSeenMyBicycle.jpeg'
+		'/images/esockhero.png'
 	)
 
 	const handleImageClick = (audioFile, imageFile) => {
@@ -96,13 +108,20 @@ function App() {
 				</SelectedImageWrapper>
 			)}
 			<Wrapper>
-				{imageArray.map(({ id, source, alt, audioFile }) => (
+				{imageArray.map(({ id, source, alt, audioFile, caption }) => (
 					<ImageWrapper key={id}>
-						<Image
-							src={source}
-							alt={alt}
-							onClick={() => handleImageClick(audioFile, source)}
-						/>
+						<SoundButton>
+							<span className='shadow'></span>
+							<span className='edge'></span>
+							<span className='front'>
+								<Image
+									src={source}
+									alt={alt}
+									onClick={() => handleImageClick(audioFile, source)}
+								/>
+								{caption}
+							</span>
+						</SoundButton>
 					</ImageWrapper>
 				))}
 
@@ -120,22 +139,103 @@ function App() {
 	)
 }
 
+const SoundButton = styled.button`
+	position: relative;
+	border: none;
+	background: transparent;
+	padding: 0;
+	cursor: pointer;
+	outline-offset: 4px;
+	transition: filter 250ms;
+
+	& .shadow {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		border-radius: 12px;
+		background: hsl(0deg 0% 0% / 0.25);
+		will-change: transform;
+		transform: translateY(2px);
+		transition: transform 600ms cubic-bezier(0.3, 0.7, 0.4, 1);
+	}
+
+	& .edge {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		border-radius: 8px;
+		background: linear-gradient(
+			to left,
+			hsl(34, 76%, 18%) 0%,
+			hsl(34, 76%, 38%) 8%,
+			hsl(34, 76%, 38%) 92%,
+			hsl(34, 76%, 8%) 100%
+		);
+	}
+
+	& .front {
+		display: block;
+		position: relative;
+		padding: 6px 12px;
+		border-radius: 8px;
+		font-size: 1rem;
+		color: white;
+		background: hsl(34, 48%, 28%);
+		will-change: transform;
+		transform: translateY(-4px);
+		transition: transform 600ms cubic-bezier(0.3, 0.7, 0.4, 1);
+		font-size: calc(10 / 16 * 1rem);
+	}
+
+	&:hover {
+		filter: brightness(110%);
+	}
+	&:hover .front {
+		transform: translateY(-6px);
+		transition: transform 250ms cubic-bezier(0.3, 0.7, 0.4, 1.5);
+	}
+	&:active .front {
+		transform: translateY(-2px);
+		transition: transform 34ms;
+	}
+	&:hover .shadow {
+		transform: translateY(4px);
+		transition: transform 250ms cubic-bezier(0.3, 0.7, 0.4, 1.5);
+	}
+	&:active .shadow {
+		transform: translateY(1px);
+		transition: transform 34ms;
+	}
+	&:focus:not(:focus-visible) {
+		outline: none;
+	}
+`
+
 const MaxWidthWrapper = styled.div`
 	max-width: 1250px;
 	margin: auto;
 `
 
 const SelectedImageWrapper = styled.div`
-  margin-bottom: 16px;
+	margin-bottom: 16px;
 `
 
 const SelectedImage = styled.img`
 	display: block;
 	object-fit: cover;
 	width: 100%;
-	max-height: 300px;
-	object-position: 50% 20%;
-  border-radius: 16px;
+	max-height: 250px;
+	object-position: 50% 10%;
+	border-radius: 16px;
+
+	@media screen and (min-width: 800px) {
+		max-height: 600px;
+    object-position: 50% 20%;
+	}
 `
 
 const Wrapper = styled.div`
@@ -143,13 +243,21 @@ const Wrapper = styled.div`
 	min-height: 100svh;
 	display: grid;
 	place-content: center;
-	gap: 16px;
+	gap: 12px;
 	grid-template-columns: repeat(auto-fill, minmax(var(--min-col-width), 1fr));
 	grid-template-rows: repeat(auto-fill, minmax(var(--min-col-width), 1fr));
 `
 
 const ImageWrapper = styled.div`
+	--outer-radius: 12px;
+	--inner-radius: 8px;
 	display: grid;
+	border-radius: var(--outer-radius);
+
+	& span {
+		text-align: center;
+		font-size: calc(12 / 16 * 1rem);
+	}
 `
 
 const Image = styled.img`
@@ -157,7 +265,8 @@ const Image = styled.img`
 	object-fit: cover;
 	width: 100%;
 	height: 100px;
-	border-radius: 8px;
+	border-radius: calc(var(--outer-radius) - var(--inner-radius));
+	object-position: 50% 10%;
 `
 
 export default App
